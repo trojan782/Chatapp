006893f6835fdbc1d440c0b9d62975b119a0f3d0
trojan782 <davidjacobs522@gmail.com>
2021-09-08 21:10:42 +0100

    created the seeder and seeded the db

--- database/seeders/ChatRoomSeeder.php
+++ database/seeders/ChatRoomSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChatRoomSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class ChatRoomSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('chat_rooms')->insert([
+            'name' => 'General'
+        ]);
+
+         DB::table('chat_rooms')->insert([
+            'name' => 'Tech Talk'
+        ]);
     }
 }
