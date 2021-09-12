40c58f7eb1013c53f83fec9fbd05383383f3572f
trojan782 <davidjacobs522@gmail.com>
2021-09-12 09:18:11 +0100

    was able to fetch rooms

--- resources/js/Pages/Chat/Container.vue
+++ resources/js/Pages/Chat/Container.vue
@@ -2,14 +2,15 @@
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Container
+                Chat
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Container
+                    <message-container/>
+                    <input-message :room="currentRoom"/>
                 </div>
             </div>
         </div>
@@ -19,11 +20,41 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
+    import MessageContainer from './MessageContainer.vue'
+    import InputMessage from './InputMessage.vue'
+    import axios from 'axios'
 
 
     export default defineComponent({
         components: {
             AppLayout,
+            MessageContainer,
+            InputMessage
+        },
+        data: function() {
+                return {
+                    chatRooms: [],
+                    currentRoom: [],
+                    messages: []
+                }
+            },
+        methods: {
+            getRooms() {
+                axios.get('/chat/rooms')
+                .then( response => {
+                    this.chatRooms = response.data
+                    this.setRoom(response.data[0])
+                })
+                .catch( error => {
+                    console.log(error)
+                })
+            },
+            setRoom( room ) {
+                this.currentRoom = room;
+            }
+        },
+        created() {
+            this.getRooms();
         },
     })
 </script>
